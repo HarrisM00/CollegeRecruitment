4550726229b2b358af445e6864fcbb4b8893b582
HarrisM00 <Harrismahmood014@gmail.com>
2024-09-21 16:58:37 -0400

    Timer set

---
+++ script.js
@@ -0,0 +1,27 @@
+function countdownTimer() {
+    const deadline = new Date("Nov 15, 2024 23:59:59").getTime();
+    
+    // today's date and time
+    const now = new Date().getTime();
+    
+    // distance between now and countdown date
+    const timeLeft = deadline - now;
+    
+    // Time calculations
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    
+    
+    document.getElementById("countdown").innerHTML = 
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+    //"Deadline Passed"
+    if (timeLeft < 0) {
+      document.getElementById("countdown").innerHTML = "Deadline Passed";
+    }
+  }
+
+  // Update every 1 second
+  setInterval(countdownTimer, 1000);
